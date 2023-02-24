8c08f4656c8e839c34dca686325471656f872649
LarsonRagnar <larionov1992@gmail.com>
2023-02-24 19:43:11 +0300

    TryParse

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вводится строчка, надо проверить все ли символы являются числами
+// 1234 подходит
+//  1234привет не подходит
+
+var stringNumber = "1234";
+
+int number; // 
+
+bool isNumber = int.TryParse(stringNumber, out number);  // Метод проверки является ли символы числами
+
+Console.WriteLine(isNumber);  // TryParse проебразование строчки в формат числа
+Console.WriteLine(number);
+
+
+
+
